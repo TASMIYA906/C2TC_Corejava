6cfed99a7148528f6e2d5839c83839317b6e6bd2
TASMIYA906 <113110467+TASMIYA906@users.noreply.github.com>
2023-01-26 15:16:46 +0530

    Add files via upload

---
+++ B28/inheritance1/inheritance1/src/com/Tasmiya/packages/Testclass.java
@@ -0,0 +1,20 @@
+package com.Tasmiya.packages;
+
+
+public class Testclass {
+	
+	
+	
+	
+	
+	
+
+        public static void main(String[] args) {
+            Hlo h = new Hlo();
+            h.design();
+        }
+
+
+
+
+}
