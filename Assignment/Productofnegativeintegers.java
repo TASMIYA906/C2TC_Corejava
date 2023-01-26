aa69841d5b354d53f9aaa8a9c519d1b758fa4bcb
TASMIYA906 <113110467+TASMIYA906@users.noreply.github.com>
2023-01-26 21:01:04 +0530

    Add files via upload

---
+++ Assignment/Productofnegativeintegers.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Productofnegativeintegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, num =10, product =1;  
+
+		for(i = 1; i<=num; ++i)  
+		{  
+		  
+		product = product*i;  
+		}  
+
+		System.out.println("product of negative integer is = " + product); 
+	}
+
+}
